0d08c19e2f2fa68e75104f34e88e6b9cec9ee39a
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-11-11 00:52:30 +0900

    動作確認用のString()関数を削除

--- dptxt/parser.go
+++ dptxt/parser.go
@@ -1,132 +1,83 @@
-package github.com/healthy-tiger/dustpan/dptxt
+package dptxt
 
 import (
 	"bufio"
-	"strings"
 	"bytes"
 	"errors"
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 	"io"
 )
 
+// エラーメッセージ
 var (
 	ErrorIllegalCharBeforeSectionName = errors.New("セクション名の前に不正な文字があります。")
-	ErrorNoSectionNamePrefix = errors.New("セクション名プリフィックスがありません。")
-	ErrorNoSectionNameSuffix = errors.New("セクション名サフィックスがありません。")
-	ErrorSectionNameIsEmpty = errors.New("セクション名が空です。")
-	ErrorUnexpectedText = errors.New("予期しない入力文字列です。")
+	ErrorNoSectionNamePrefix          = errors.New("セクション名プリフィックスがありません。")
+	ErrorNoSectionNameSuffix          = errors.New("セクション名サフィックスがありません。")
+	ErrorSectionNameIsEmpty           = errors.New("セクション名が空です。")
+	ErrorUnexpectedText               = errors.New("予期しない入力文字列です。")
 )
 
 const defaultSectionNameLen = 20
 const defaultSectionParagraphNum = 20
 const defaultParagraphLineNum = 10
 
-
-func IsSp(r rune) bool {
-	if r == ' ' || r == '　' {
+func isSp(r rune) bool {
+	if r == ' ' || r == '　' || r == '\t' {
 		return true
 	}
 	return false
 }
 
-func IsNonSp(r rune) bool {
-	return !IsSp(r)
+func isNonSp(r rune) bool {
+	return !isSp(r)
 }
 
-func IsAt(r rune) bool {
+func isAt(r rune) bool {
 	if r == '@' || r == '＠' {
 		return true
 	}
 	return false
 }
 
-func IsNonAt(r rune) bool {
-	return !IsAt(r)
+func isNonAt(r rune) bool {
+	return !isAt(r)
 }
 
-func IsColon(r rune) bool {
+func isColon(r rune) bool {
 	if r == ':' || r == '：' {
 		return true
 	}
 	return false
 }
 
-func IsNonColon(r rune) bool {
-	return !IsColon(r)
+func isNonColon(r rune) bool {
+	return !isColon(r)
 }
 
 type Document struct {
 	Filename string
-	Sections []*Section
+	Sections []Section
 }
 
 type Section struct {
-	Name []byte
+	Name  []byte
 	Value []Paragraph
-	IsCompound bool
 }
 
 type Paragraph struct {
 	Value [][]byte
 }
 
-func (d *Document) String() string {
-	var b strings.Builder
-	b.WriteString("-------document-------\n")
-	b.WriteString("filename=")
-	b.WriteString(d.Filename)
-	b.WriteString("\n")
-	for _, s := range d.Sections {
-		b.WriteString(s.String())
-		b.WriteString("\n")
-	}
-	b.WriteString("---------end----------")
-	return b.String()
-}
-
-func (s *Section) String() string {
-	var b strings.Builder
-	b.WriteString("-----section-----\n")
-	if s.IsCompound {
-		b.WriteString("name*=")
-	} else {
-		b.WriteString("name =")
-	}
-	b.Write(s.Name)
-	b.WriteString("\n")
-	for _, p := range s.Value {
-		b.WriteString(p.String())
-		b.WriteString("\n")
-	}
-	b.WriteString("-------end-------")
-	return b.String()
-}
-
-func (p *Paragraph) String() string {
-	var b strings.Builder
-	b.WriteString("--paragraph--\n")
-	for _, v := range p.Value {
-		b.Write(v)
-		b.WriteString("\n")
-	}
-	b.WriteString("-----end-----")
-	return b.String()
-}
-
-type LineScanner struct {
-	scanner *bufio.Scanner
+type lineScanner struct {
+	scanner  *bufio.Scanner
 	lastline []byte
-	unread bool
+	unread   bool
 }
 
-func NewLineScanner(r io.Reader) *LineScanner {
-	return &LineScanner{ bufio.NewScanner(r), nil, false }
+func newLineScanner(r io.Reader) *lineScanner {
+	return &lineScanner{bufio.NewScanner(r), nil, false}
 }
 
-func (ls *LineScanner) nextLine() ([]byte, error) {
+func (ls *lineScanner) nextLine() ([]byte, error) {
 	if ls.unread && ls.lastline != nil {
 		ls.unread = false
 		return ls.lastline, nil
@@ -147,11 +98,11 @@ func (ls *LineScanner) nextLine() ([]byte, error) {
 }
 
 // 空白行を読み飛ばす。
-func (ls *LineScanner) SkipEmptyLines() (int, error) {
+func (ls *lineScanner) SkipEmptyLines() (int, error) {
 	n := 0
 	line, err := ls.nextLine()
 	for err == nil {
-		line = bytes.TrimFunc(line, IsSp)
+		line = bytes.TrimLeftFunc(line, isSp)
 		if len(line) > 0 {
 			break
 		}
@@ -165,7 +116,7 @@ func (ls *LineScanner) SkipEmptyLines() (int, error) {
 	return n, nil
 }
 
-func (ls *LineScanner) UnreadLine() bool {
+func (ls *lineScanner) UnreadLine() bool {
 	// 戻せるのは一行分だけ。
 	// すでに戻してある分がある場合は、エラーになる。
 	if ls.unread {
@@ -175,66 +126,64 @@ func (ls *LineScanner) UnreadLine() bool {
 	return true
 }
 
-func processSection(ls *LineScanner) (*Section, error)  {
+func processSection(ls *lineScanner) (*Section, error) {
 	// 空白行を読み飛ばす。
 	ls.SkipEmptyLines()
 	line, err := ls.nextLine()
 	if err != nil {
 		return nil, err
 	}
-	line = bytes.TrimFunc(line, IsSp)
+	line = bytes.TrimLeftFunc(line, isSp)
 
 	// セクション名の始まり
-	i := bytes.IndexFunc(line, IsAt)  
+	i := bytes.IndexFunc(line, isAt)
 	if i != 0 {
 		return nil, ErrorNoSectionNamePrefix
 	}
 	line = line[i:]
-	i = bytes.IndexFunc(line, IsNonAt)
+	i = bytes.IndexFunc(line, isNonAt)
 	if i == -1 {
 		return nil, ErrorSectionNameIsEmpty
 	}
 	line = line[i:]
-	
+
 	// セクション名の終わり
-	i = bytes.IndexFunc(line, IsColon)
+	i = bytes.IndexFunc(line, isColon)
 	if i == -1 { // コロンが見つからない
 		return nil, ErrorNoSectionNameSuffix
 	}
-	name := bytes.TrimFunc(line[:i], IsSp)
-	if len(name) == 0 { // セクション名が長さ0
+	name := bytes.TrimFunc(line[:i], isSp) // セクション名の両端の空白を切り捨てる。
+	if len(name) == 0 {                    // セクション名が長さ0
 		return nil, ErrorSectionNameIsEmpty
 	}
 	line = line[i:]
 
 	// セクション本文の始まり
-	i = bytes.IndexFunc(line, IsNonColon)
-	if i == -1 {
-		// 複合セクション
-		ps, err := readCompoundValues(ls)
-		if err != nil {
-			return nil, err
-		}
-		return &Section{ name, ps, true }, nil
-	} else {
-		// 単純セクション
-
+	i = bytes.IndexFunc(line, isNonColon)
+	var head []byte = nil
+	if i != -1 {
 		// lineをトリミングしてあるので、必ずなにか文字列があるはず。
-		value := bytes.TrimFunc(line[i:], IsSp)
-		return &Section{ name, []Paragraph{ Paragraph{ [][]byte{ value } } }, false }, nil
+		head = bytes.TrimFunc(line[i:], isSp)
 	}
-
+	ps, err := readCompoundValues(ls, head)
+	if err != nil {
+		return nil, err
+	}
+	return &Section{name, ps}, nil
 }
 
-func readCompoundValues(ls *LineScanner) ([]Paragraph, error) {
+func readCompoundValues(ls *lineScanner, head []byte) ([]Paragraph, error) {
 	values := make([]Paragraph, 0, defaultSectionParagraphNum)
 	pvalues := make([][]byte, 0, defaultParagraphLineNum)
+	if head != nil {
+		pvalues = append(pvalues, head)
+	}
 
 	line, err := ls.nextLine()
 	for err == nil {
-		line = bytes.TrimFunc(line, IsSp)
+		line = bytes.TrimFunc(line, isSp)
 		if len(line) > 0 {
-			if bytes.IndexFunc(line, IsAt) == 0 { // 次のセクションまで来た。
+			if bytes.IndexFunc(line, isAt) == 0 { // 次のセクションまで来た。
 				ls.UnreadLine()
 				break
 			} else {
@@ -242,7 +191,7 @@ func readCompoundValues(ls *LineScanner) ([]Paragraph, error) {
 			}
 		} else {
 			if len(pvalues) > 0 {
-				values = append(values, Paragraph{ pvalues })
+				values = append(values, Paragraph{pvalues})
 				pvalues = make([][]byte, 0, defaultParagraphLineNum)
 			}
 		}
@@ -254,46 +203,23 @@ func readCompoundValues(ls *LineScanner) ([]Paragraph, error) {
 	}
 
 	if len(pvalues) > 0 {
-		values = append(values, Paragraph{ pvalues })
+		values = append(values, Paragraph{pvalues})
 	}
 	return values, nil
 }
 
-func ProcessFile(filename string) (*Document, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
+func ParseDocument(filename string, r io.Reader) (*Document, error) {
+	ls := newLineScanner(r)
 
-	ls := NewLineScanner(f)
-
-	secs := make([]*Section, 0, 10)
+	secs := make([]Section, 0, 10)
 	var sec *Section
-	sec, err = processSection(ls)
+	sec, err := processSection(ls)
 	for err == nil {
-		secs = append(secs, sec)
+		secs = append(secs, *sec)
 		sec, err = processSection(ls)
 	}
 	if err != io.EOF {
 		return nil, err
 	}
-	return &Document{ filename, secs }, nil
-}
-
-func main() {
-	for i := 1; i < len(os.Args); i++ {
-		if paths, err := filepath.Glob(os.Args[i]); err != nil {
-			log.Fatal(err)
-		} else {
-			for _, p := range paths {
-				doc, err := ProcessFile(p)
-				if err != nil {
-					log.Println(p, err)
-				} else {
-					fmt.Println(doc)
-				}
-			}
-		}
-	}
+	return &Document{filename, secs}, nil
 }
