5e57151633fb25c46f5167e8be8b7f9aca7f6806
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-11-25 22:52:01 +0900

    不要なファイルを削除

--- dpsh/testdata/test.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	let updateElm = document.createElement('div');
-	updateElm.innerHTML = document.body.dataset["update"];
-
-	document.body.insertBefore(updateElm, document.body.firstChild);
-}
