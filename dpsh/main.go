9c7a4fe1f4bd9fecb1f351749f582e8b70bcc3d5
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-12-04 00:21:33 +0900

    ヘルプメッセージをちょっと変更

--- dpsh/main.go
+++ dpsh/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
+func Usage() {
+	fmt.Fprintf(flag.CommandLine.Output(), "Dustpan Shell\nUsage:\n")
+	flag.PrintDefaults()
+}
+
 func main() {
+	flag.Usage = Usage
+	
 	var configpath string
 	flag.StringVar(&configpath, "c", "config.json", "config file path")
 	flag.Parse()
