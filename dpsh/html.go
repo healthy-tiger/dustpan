8528e392e8a9d4d088b6e56b018ba9ad65c178f5
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2020-09-21 08:46:02 +0900

    日付を選択可能なテキストとしてHTMLに出力するように変更した。

--- dpsh/html.go
+++ dpsh/html.go
@@ -22,9 +22,10 @@ var scriptClose = []byte("</script>")
 
 var divErrFmt string = `<div class="dp-err" data-msg="%v"></div>`
 var divExpire []byte = []byte(`<div class="dp-expired"></div>`)
-var divDateFmt string = `<div class="dp-date" data-year="%v" data-month="%v" data-day="%v"></div>`
-var divDateWithSuffixFmt string = `<div class="dp-date" data-year="%v" data-month="%v" data-day="%v" data-suffix="%v"></div>`
-var divDateExpiredFmt string = `<div class="dp-date dp-expired" data-year="%v" data-month="%v" data-day="%v"></div>`
+var divSecDateFmt string = `<div class="dp-date" data-year="%v" data-month="%v" data-day="%v">%d/%02d/%02d</div>`
+var divSecDateExpiredFmt string = `<div class="dp-date dp-expired" data-year="%d" data-month="%d" data-day="%d">%d/%02d/%02d</div>`
+var divParaDateFmt string = `<div class="dp-date" data-year="%v" data-month="%v" data-day="%v">(%d/%02d/%02d)</div>`
+var divParaDateWithSuffixFmt string = `<div class="dp-date" data-year="%d" data-month="%d" data-day="%d" data-suffix="%v">(%d/%02d/%02d %v)</div>`
 
 var pOpen []byte = []byte(`<div class="dp-p">`)
 var pClose []byte = []byte("</div>")
@@ -51,7 +52,7 @@ var contentOpen1 string = `<!DOCTYPE html>
 <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
 <title>%s</title>`
 
-var defaultstyle []byte = []byte(`body{background-color:#fff}body,html{padding:0;margin:0}body{font-family:Meiryo UI;font-size:9pt}.dp-heading{font-size:2em;margin:10pt;display:flex}.dp-heading>.dp-title{flex:initial}.dp-heading>.dp-update{font-size:.5em;flex:auto;text-align:right}.dp-heading>.dp-title:after{content:attr(data-title)}.dp-heading>.dp-update:after{content:attr(data-date) " "attr(date-time) " 更新"}.dp-t .dp-h{width:100%;font-weight:700}.dp-t,.dp-t .dp-b{width:100%}.dp-t .dp-r{width:100%;display:flex;justify-content:stretch;flex-wrap:nowrap;flex-direction:row;align-items:stretch}.dp-t .dp-r>.dp-c{flex-shrink:0;padding:3pt}.dp-t>.dp-b>.dp-r:nth-child(n+2){border-style:solid;border-color:#999;border-width:1px 0 0}.dp-t .dp-r>.dp-c:nth-child(n+2){border-style:solid;border-color:#999;border-width:0 0 0 1px}.dp-t .dp-h .dp-r{white-space:nowrap;vertical-align:bottom;text-align:center;border-bottom-width:3px;border-bottom-style:double;border-bottom-color:#999}.dp-t>.dp-b>.dp-r>.dp-c{vertical-align:top}.dp-t>.dp-b>.dp-r>.dp-c:empty{background-color:#eee;text-align:center}.dp-t .dp-b .dp-r .dp-c:empty:before{content:"?"}.dp-t>.dp-b>.dp-r>.dp-c .dp-err{display:inline-block;background-color:red;color:#fff;font-weight:700;font-size:.8em;padding:.1em}.dp-t>.dp-b>.dp-r>.dp-c .dp-err:before{content:"エラー："}.dp-t>.dp-b>.dp-r>.dp-c .dp-err:after{content:attr(data-msg)}.dp-t>.dp-b>.dp-r>.dp-c>.dp-date{text-align:center}.dp-t>.dp-b>.dp-r>.dp-c>.dp-date.dp-expired{color:red;font-weight:700}.dp-t>.dp-b>.dp-r>.dp-c>.dp-date.dp-expired:after,.dp-t>.dp-b>.dp-r>.dp-c>.dp-date:after{content:attr(data-year) "/"attr(data-month) "/"attr(data-day)}.dp-t>.dp-b>.dp-r>.dp-c .dp-p{padding-top:1.5em}.dp-t>.dp-b>.dp-r>.dp-c .dp-p:first-child{padding-top:0}.dp-t>.dp-b>.dp-r>.dp-c .dp-p:last-child{padding-bottom:0}.dp-t>.dp-b>.dp-r>.dp-c .dp-p>.dp-date{display:inline;margin-left:.5em}.dp-t>.dp-b>.dp-r>.dp-c .dp-p>.dp-date:after{content:"("attr(data-year) "/"attr(data-month) "/"attr(data-day) ")"}.dp-t>.dp-b>.dp-r>.dp-c .dp-p>.dp-date[data-suffix]:after{content:"("attr(data-year) "/"attr(data-month) "/"attr(data-day) " "attr(data-suffix) ")"}@media print{body,html{margin:0;padding:0}.dp-heading{display:none}.dp-t{font-size:7pt;border:1px solid #999;box-sizing:border-box}.dp-t .dp-h{break-inside:avoid}.dp-t .dp-b .dp-r{break-inside:auto}.dp-t .dp-b .dp-r .dp-c .dp-p{break-inside:avoid}.dp-t .dp-b .dp-r .dp-c:empty{background-color:transparent}.dp-t .dp-b .dp-r .dp-c .dp-err{display:none}}`)
+var defaultstyle []byte = []byte(`body{background-color:#fff}body,html{padding:0;margin:0}body{font-family:Meiryo UI;font-size:9pt}.dp-heading{font-size:2em;margin:10pt;display:flex}.dp-heading>.dp-title{flex:initial}.dp-heading>.dp-update{font-size:.5em;flex:auto;text-align:right}.dp-heading>.dp-title:after{content:attr(data-title)}.dp-heading>.dp-update:after{content:attr(data-date) " "attr(date-time) " 更新"}.dp-t .dp-h{width:100%;font-weight:700}.dp-t,.dp-t .dp-b{width:100%}.dp-t .dp-r{width:100%;display:flex;justify-content:stretch;flex-wrap:nowrap;flex-direction:row;align-items:stretch}.dp-t .dp-r>.dp-c{flex-shrink:0;padding:3pt}.dp-t>.dp-b>.dp-r:nth-child(n+2){border-style:solid;border-color:#999;border-width:1px 0 0}.dp-t .dp-r>.dp-c:nth-child(n+2){border-style:solid;border-color:#999;border-width:0 0 0 1px}.dp-t .dp-h .dp-r{white-space:nowrap;vertical-align:bottom;text-align:center;border-bottom-width:3px;border-bottom-style:double;border-bottom-color:#999}.dp-t>.dp-b>.dp-r>.dp-c{vertical-align:top}.dp-t>.dp-b>.dp-r>.dp-c:empty{background-color:#eee;text-align:center}.dp-t .dp-b .dp-r .dp-c:empty:before{content:"?"}.dp-t>.dp-b>.dp-r>.dp-c .dp-err{display:inline-block;background-color:red;color:#fff;font-weight:700;font-size:.8em;padding:.1em}.dp-t>.dp-b>.dp-r>.dp-c .dp-err:before{content:"エラー："}.dp-t>.dp-b>.dp-r>.dp-c .dp-err:after{content:attr(data-msg)}.dp-t>.dp-b>.dp-r>.dp-c>.dp-date{text-align:center}.dp-t>.dp-b>.dp-r>.dp-c>.dp-date.dp-expired{color:red;font-weight:700}.dp-t>.dp-b>.dp-r>.dp-c .dp-p{padding-top:1.5em}.dp-t>.dp-b>.dp-r>.dp-c .dp-p:first-child{padding-top:0}.dp-t>.dp-b>.dp-r>.dp-c .dp-p:last-child{padding-bottom:0}.dp-t>.dp-b>.dp-r>.dp-c .dp-p>.dp-date{display:inline}@media print{body,html{margin:0;padding:0}.dp-heading{display:none}.dp-t{font-size:7pt;border:1px solid #999;box-sizing:border-box}.dp-t .dp-h{break-inside:avoid}.dp-t .dp-b .dp-r{break-inside:auto}.dp-t .dp-b .dp-r .dp-c .dp-p{break-inside:avoid}.dp-t .dp-b .dp-r .dp-c:empty{background-color:transparent}.dp-t .dp-b .dp-r .dp-c .dp-err{display:none}}`)
 var defaultColumnWithWidth string = ".dp-c[data-section=\"%s\"]{flex-grow:0;flex-basis:%s;width:%s;}"
 var defaultColumnWithoutWidth string = ".dp-c[data-section=\"%s\"]{flex-grow:1;width:0px;}"
 
@@ -96,9 +97,16 @@ func htmlWriteParagraph(para *dptxt.Paragraph, w *bufio.Writer) error {
 	if para.Time != nil {
 		year, month, day := para.Time.Date()
 		if byteIsNilOrEmpty(para.TimeSuffix) {
-			_, err = w.WriteString(fmt.Sprintf(divDateFmt, year, int(month), day))
+			_, err = w.WriteString(fmt.Sprintf(divParaDateFmt, year, int(month), day, year, int(month), day))
 		} else {
-			_, err = w.WriteString(fmt.Sprintf(divDateWithSuffixFmt, year, int(month), day, html.EscapeString(string(para.TimeSuffix))))
+			_, err = w.WriteString(fmt.Sprintf(divParaDateWithSuffixFmt, year,
+				int(month),
+				day,
+				html.EscapeString(string(para.TimeSuffix)),
+				year,
+				int(month),
+				day,
+				html.EscapeString(string(para.TimeSuffix))))
 		}
 		if err != nil {
 			return err
@@ -134,9 +142,9 @@ func htmlWriteSection(sec *dptxt.Section, secname string, w *bufio.Writer) error
 		if sec.Time != nil {
 			year, month, day := sec.Time.Date()
 			if sec.Expired {
-				_, err = w.WriteString(fmt.Sprintf(divDateExpiredFmt, year, int(month), day))
+				_, err = w.WriteString(fmt.Sprintf(divSecDateExpiredFmt, year, int(month), day, year, int(month), day))
 			} else {
-				_, err = w.WriteString(fmt.Sprintf(divDateFmt, year, int(month), day))
+				_, err = w.WriteString(fmt.Sprintf(divSecDateFmt, year, int(month), day, year, int(month), day))
 			}
 			if err != nil {
 				return err
